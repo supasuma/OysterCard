949a0ee7b3af4e34a7a69964986fa09c5a7a15d4
James Stonehill <jamesstonehill@gmail.com>
2016-08-22 14:40:09 +0100

    First test written and passing (oyster balance starts at 0 be default)

---
+++ lib/oyster.rb
@@ -0,0 +1,12 @@
+
+
+class Oyster
+  attr_reader :balance
+  STARTING_BALANCE = 0.0
+
+  def initialize(balance = STARTING_BALANCE)
+    @balance = balance
+  end
+
+
+end
--- spec/oystercar_spec.rb
+++ spec/oystercar_spec.rb
@@ -1,4 +1,9 @@
+require 'oyster.rb'
+
 describe Oyster do
 
+  it 'is initialised with a balence of 0' do
+    expect(subject.balance).to eq(0)
+  end
 
 end
