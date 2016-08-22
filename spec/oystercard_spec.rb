eb37751f609c17ce6aa8e22086135d410c012938
Tim3tang <Tim3tang@gmail.com>
2016-08-22 17:26:50 +0100

    ch4: add the balance changes

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,11 @@
+require 'oystercard'
+
 describe Oystercard do
-  
+    subject(:oystercard) {described_class.new}
+
+  describe '#card' do
+    it 'has a balance of zero' do
+      expect(oystercard.balance).to eq 0
+    end
+  end
 end
