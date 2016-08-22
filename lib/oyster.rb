5bb4146582efa71b2f121e1dd272b8559a55b3d4
James Stonehill <jamesstonehill@gmail.com>
2016-08-22 15:06:36 +0100

    added touch in and out capability + tests

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -1,12 +1,23 @@
 
 
 class Oyster
-  attr_reader :balance
+  attr_reader :balance, :in_journey
+  alias in_journey? in_journey
+
   STARTING_BALANCE = 0.0
   MAX_BALANCE = 90.0
 
   def initialize(balance = STARTING_BALANCE)
     @balance = balance
+    @in_journey = false
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -14,5 +25,7 @@ class Oyster
     @balance += amount
   end
 
-
+  def deduct(amount)
+    @balance -= amount
+  end
 end
--- spec/oystercar_spec.rb
+++ spec/oystercar_spec.rb
@@ -1,17 +1,48 @@
 require 'oyster.rb'
 
 describe Oyster do
-
-  it 'is initialised with a balence of 0' do
-    expect(subject.balance).to eq(0)
+  context 'initialize' do
+    it 'is initialised with a balence of 0' do
+      expect(subject.balance).to eq(0)
+    end
   end
 
-  it "is able to take new currency value" do
-    expect{subject.top_up(10)}.to change{subject.balance}.by 10
+  context '#top_up' do
+    it "cant hold a balance of more than 90" do
+      expect{subject.top_up(91)}.to raise_error "Oyster max balance is 90.0."
+    end
+
+    it "is able to take new currency value" do
+      expect{subject.top_up(10)}.to change{subject.balance}.by 10
+    end
   end
 
-  it "cant hold a balance of more than 90" do
-    expect{subject.top_up(91)}.to raise_error "Oyster max balance is 90.0."
+  context '#deduct' do
+    it 'will deduct the balance by a given amount' do
+      expect{subject.deduct(10)}.to change{subject.balance}.by -10
+    end
   end
 
+  context '#in_journey?' do
+    it 'starts out as not being on a journey' do
+      expect(subject.in_journey?).to eq(false)
+    end
+
+    it 'changes to being on a journey when you touch in' do
+      subject.touch_in
+      expect(subject.in_journey?).to eq(true)
+    end
+  end
+
+  context '#touch_in' do
+    it 'is possible to touch_in' do
+      expect(subject).to respond_to(:touch_in)
+    end
+  end
+
+  context '#touch_out' do
+    it 'is possible to touch_out' do
+      expect(subject).to respond_to(:touch_out)
+    end
+  end
 end
