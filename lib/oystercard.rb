0ee3b32c11d167e8d90706d54bbc4d11170391ea
Tim3tang <Tim3tang@gmail.com>
2016-08-22 19:31:29 +0100

    ch8 in_journey, touch_in and touch_out added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,19 +1,38 @@
 class Oystercard
 
-attr_reader :balance
+attr_reader :balance, :in_journey
 
 LIMIT = 90
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
-    fail "Limit £#{LIMIT} exceeded" if @balance + amount > LIMIT
+    fail "Limit £#{LIMIT} exceeded" if full?(amount)
     @balance += amount
   end
 
   def deduct(amount)
     @balance -= amount
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  private
+
+  def full?(amount)
+    @balance + amount > LIMIT
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,22 +8,43 @@ describe Oystercard do
       expect(oystercard.balance).to eq 0
     end
   end
+
+  describe '#in_journey?' do
+    it 'not in journey initially' do
+      expect(oystercard.in_journey?).to be false
+    end
+  end
+
   describe '#top_up' do
+
+    context 'when under limit' do
     it 'responds to top-up with one argument' do
       expect(oystercard).to respond_to(:top_up).with(1).argument
     end
     it 'can increase the balance' do
       expect{oystercard.top_up 10}.to change{oystercard.balance}.by 10
     end
+  end
 
+    context 'when over limit' do
     it 'raises an error when more than £90 is added' do
       LIMIT = Oystercard::LIMIT
       oystercard.top_up(LIMIT)
       expect{ oystercard.top_up 1 }.to raise_error "Limit £#{LIMIT} exceeded"
     end
-   end
+  end
+  end
 
-   describe '#deduct' do
+  context 'when travelling' do
+
+    describe '#touch_in' do
+      it 'can touch in' do
+        oystercard.touch_in
+        expect(oystercard).to be_in_journey
+      end
+    end
+
+    describe '#deduct' do
      it 'respond to deduct with one argument' do
        expect(oystercard).to respond_to(:deduct).with(1).argument
      end
@@ -32,4 +53,12 @@ describe Oystercard do
      end
    end
 
+   describe '#touch_out' do
+     it 'can touch out' do
+       oystercard.touch_in
+       oystercard.touch_out
+       expect(oystercard).not_to be_in_journey
+     end
+   end
+ end
 end
