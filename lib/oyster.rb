94d59c8b702798119a7abeedb2d8a70eaddc5c7a
MrJons <shad_stick@hotmail.co.uk>
2016-08-22 14:55:05 +0100

    Balance limit added.

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -3,9 +3,15 @@
 class Oyster
   attr_reader :balance
   STARTING_BALANCE = 0.0
+  MAX_BALANCE = 90.0
 
   def initialize(balance = STARTING_BALANCE)
     @balance = balance
+  end
+
+  def top_up(amount)
+    fail "Oyster max balance is #{MAX_BALANCE}." if @balance + amount > MAX_BALANCE
+    @balance += amount
   end
 
 
--- spec/oystercar_spec.rb
+++ spec/oystercar_spec.rb
@@ -6,4 +6,12 @@ describe Oyster do
     expect(subject.balance).to eq(0)
   end
 
+  it "is able to take new currency value" do
+    expect{subject.top_up(10)}.to change{subject.balance}.by 10
+  end
+
+  it "cant hold a balance of more than 90" do
+    expect{subject.top_up(91)}.to raise_error "Oyster max balance is 90.0."
+  end
+
 end
