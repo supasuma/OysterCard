566bd1a607379bf6a4d9c48598dcb1f893419a0b
Thao Vo <ttthao.vo@gmail.com>
2016-08-24 11:33:48 +0100

    2nd station test failed

---
+++ lib/station.rb
@@ -0,0 +1,8 @@
+class Station
+ attr_reader :station
+ 
+  def initalize(station)
+    @station = station
+  end
+
+end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,11 +2,11 @@ require 'station'
 
 describe Station do
 
-it 'exposes station variable' do
+  it 'exposes station variable' do
+    expect(subject).to respond_to(:station)
+  end
 
-  expect(subject).to respond_to(:station)
-
-end
-
-
+  it 'exposes zone variable' do
+    expect(subject).to respond_to(:zone)
+  end
 end
